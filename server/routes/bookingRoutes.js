80855e7aaf3b6f300d5b58296531b0c7b0e3a2c9
phenalinpant <phenalin.pant@deerwalk.edu.np>
2026-02-17 14:25:30 +0545

    Updated booking and payment functionality

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth.js";
-import { createBooking, getOccupiedSeats, getMyBookings } from "../controllers/BookingController.js";
+import { createBooking, getOccupiedSeats, getMyBookings, payWithKhalti } from "../controllers/BookingController.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.post("/create", authMiddleware, createBooking); // create booking
 bookingRouter.get("/my", authMiddleware, getMyBookings);      // get logged-in user bookings
 bookingRouter.get("/seats/:showId", getOccupiedSeats);
+bookingRouter.post("/khalti", payWithKhalti)
 
 export default bookingRouter;
